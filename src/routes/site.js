319a180c78c5509f7f898e3a005adf1fc963c8cb
quangndgcc200030 <91590378+quangndgcc200030@users.noreply.github.com>
2022-10-18 15:38:08 +0700

    fix some bug

--- src/routes/site.js
+++ src/routes/site.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../app/middlewares/AuthMiddleware')
 
 router.get('/logout', authMiddleware.loggedin, siteController.logout)
 router.get('/manage', authMiddleware.admin, siteController.admin)
-router.get('/view-detail/:id', authMiddleware.loggedin, siteController.viewdetail)
+router.get('/view-detail/:id', authMiddleware.freedom, siteController.viewdetail)
 router.get('/about', authMiddleware.freedom, siteController.about)
 router.get('/shop', authMiddleware.freedom, siteController.shop)
 router.get('/', authMiddleware.freedom, siteController.index)
